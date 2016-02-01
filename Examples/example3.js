4f61415c458451e1fb9ca959552fed2adde68708
ibmwtx <sdandamu@us.ibm.com>
2016-02-01 14:12:38 -0500

    Removed Copyright

--- Examples/example3.js
+++ Examples/example3.js
@@ -1,14 +1,3 @@
-
-/* 
- * Copyright (C) 2006 - 2014 IBM Corporation.  All rights reserved.
- *
- * This software is the property of IBM Corporation and its
- * licensors and contains their confidential trade secrets.  Use, examination,
- * copying, transfer and disclosure to others, in whole or in part, are
- * prohibited except with the express prior written consent of
- * IBM Corporation.
- *
- */
  	var ref = require('ref');
 	var ffi = require('ffi');
     var general = require('./general.js');
